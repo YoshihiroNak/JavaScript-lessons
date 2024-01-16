e7914f110a249003415272b734a3fbaa81fd8c9d
YoshihiroNak <nakagawasaiyoshihiro@gmail.com>
2024-01-16 16:16:01 +1000

    Fixed codes

---
+++ script.js
@@ -0,0 +1,43 @@
+// const el = document.querySelectorAll('li')
+// console.log(el)
+// // el.innerHTML = 'Hello <span style="color: red">World!</span>'
+
+// const newDiv = document.createElement('div')
+// // console.log(newDiv)
+// newDiv.innerHTML = '<h3>Awesome content!</h3>'
+// newDiv.id = 'spam'
+// newDiv.style.color = 'blue'
+// document.body.insertBefore(newDiv, document.querySelector('ul'))
+// const myColor = 'blue'
+// document.body.innerHTML += `<div id="spam" style="color: ${myColor}"><h3>Awesome content</h3></div>`
+const items = [
+    'Adding to the DOM',
+    'Quering the DOM',
+    'Changing the DOM',
+    'Event Listeners'
+]
+
+const ul = document.querySelector('ul')
+
+// for(let item of items) {
+//     ul.innerHTML += `<li>${item}</li>`
+//     // const newLi = document.createElement('li')
+//     // newLi.innerText = item
+//     // ul.appendChild(newLi)
+// }
+
+// React needs this way
+const lis = items.map(item => `<li>${item}</li>`)
+ul.innerHTML = lis.join('')
+
+// Handle a mouse click on the h1 element
+// document.querySelector('h1').addEventListener('click', event => event.target.innerHTML += '!')
+
+const newItem = document.querySelector('#newItem')
+const btn = document.querySelector('button')
+
+btn.addEventListener('click', () => {
+    ul.innerHTML += `<li>${newItem.value}</li>`
+    newItem.value = ''
+})
+
